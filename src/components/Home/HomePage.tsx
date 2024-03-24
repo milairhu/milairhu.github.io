11c91479885183a14f1f153670f90eab96c5e3c8
milairhu <hugo.milair@gmail.com>
2024-03-24 20:12:11 +0100

    SideNav is centered

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import SideNav from '../SideNav/SideNav';
 const HomePage = () => {
   return (
-    <div className="flex h-full w-full bg-slate-800">
-      <div className='h-full'>
+    <div className="flex h-full w-full bg-slate-800 items-center ">
+      <div className='h-2/6 ml-8'>
         <SideNav />
       </div>
       
-      <div className="flex-grow h-full w-full">
+      <div className=" h-full w-full">
         {/* Contenu de la partie du portfolio */}
       </div>
     </div>
--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as ContactSVG} from '../../icons/contact.svg';
 
 const SideNav = () => {
   return (
-      <div className=" bg-slate-300 rounded-full h-full py-4 px-3 flex flex-col justify-evenly">
+      <div className=" bg-slate-300 rounded-full h-full  px-3 flex flex-col justify-evenly">
         <DiplomaSVG className="w-10 h-10" />
         <ProjectsSVG className="w-10 h-10" />
         <ContactSVG className="w-10 h-10" />
