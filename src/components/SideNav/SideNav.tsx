5bd0c473cdb7c647f2ed4daae4b6312fce3e21ae
milairhu <hugo.milair@gmail.com>
2024-03-24 20:18:05 +0100

    SideNav: cursos-pointer and useState

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as DiplomaSVG} from '../../icons/diploma.svg';
 import { ReactComponent as ProjectsSVG} from '../../icons/projects.svg';
 import { ReactComponent as ContactSVG} from '../../icons/contact.svg';
 
+enum SideNavItems {
+  DIPLOMA = 'diploma',
+  PROJECTS = 'projects',
+  CONTACT = 'contact',
+}
+
 const SideNav = () => {
+
+  const [curr, setCurr] = useState<SideNavItems>(SideNavItems.DIPLOMA);
+
   return (
-      <div className=" bg-slate-300 rounded-full h-full  px-3 flex flex-col justify-evenly">
-        <DiplomaSVG className="w-10 h-10" />
-        <ProjectsSVG className="w-10 h-10" />
-        <ContactSVG className="w-10 h-10" />
+      <div className=" bg-slate-300 rounded-full h-full px-3 flex flex-col justify-evenly">
+        <DiplomaSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.DIPLOMA)} />
+        <ProjectsSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.PROJECTS)} />
+        <ContactSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.CONTACT)}/>
       </div>
   );
 }
