3c67007435e1b36102be86729b97272a49573f97
milairhu <hugo.milair@gmail.com>
2024-03-29 18:29:32 +0100

    White circles behind navBar items

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ReactComponent as DiplomaSVG} from '../../icons/diploma.svg';
 import { ReactComponent as ProjectsSVG} from '../../icons/projects.svg';
 import { ReactComponent as ContactSVG} from '../../icons/contact.svg';
@@ -13,12 +13,23 @@ const SideNav = () => {
 
   const [curr, setCurr] = useState<SideNavItems>(SideNavItems.DIPLOMA);
 
+  useEffect(() => {
+    console.log(curr);
+
+  }, [curr]);
+
   return (
       <div className=" bg-slate-300 rounded-full h-full px-3 flex flex-col justify-evenly">
-        <DiplomaSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.DIPLOMA)} />
-        <ProjectsSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.PROJECTS)} />
-        <ContactSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.CONTACT)}/>
-      </div>
+        <div className={`${curr === SideNavItems.DIPLOMA && 'bg-white'} rounded-full p-3`}>
+          <DiplomaSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.DIPLOMA)} />
+        </div>
+        <div className={`${curr === SideNavItems.PROJECTS && 'bg-white'} rounded-full p-3`}>
+          <ProjectsSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.PROJECTS)} />
+        </div>
+        <div className={`${curr === SideNavItems.CONTACT && 'bg-white'} rounded-full p-3`}>
+          <ContactSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.CONTACT)}/>
+        </div>
+    </div>
   );
 }
 
