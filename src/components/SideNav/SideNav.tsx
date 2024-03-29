cc9a40da56f5cc124246a21b1560a98acf3363a2
milairhu <hugo.milair@gmail.com>
2024-03-29 18:39:57 +0100

    Replace Contacts by Home

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { ReactComponent as DiplomaSVG} from '../../icons/diploma.svg';
 import { ReactComponent as ProjectsSVG} from '../../icons/projects.svg';
-import { ReactComponent as ContactSVG} from '../../icons/contact.svg';
+import { ReactComponent as HomeSVG  } from '../../icons/home.svg';
 
 enum SideNavItems {
   DIPLOMA = 'diploma',
   PROJECTS = 'projects',
-  CONTACT = 'contact',
+  HOME = 'home'
 }
 
 const SideNav = () => {
 
-  const [curr, setCurr] = useState<SideNavItems>(SideNavItems.DIPLOMA);
+  const [curr, setCurr] = useState<SideNavItems>(SideNavItems.HOME);
 
   useEffect(() => {
     console.log(curr);
@@ -20,14 +20,14 @@ const SideNav = () => {
 
   return (
       <div className=" bg-slate-300 rounded-full h-full px-3 flex flex-col justify-evenly">
+        <div className={`${curr === SideNavItems.HOME && 'bg-white'} rounded-full p-3`}>
+          <HomeSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.HOME)}/>
+        </div>
         <div className={`${curr === SideNavItems.DIPLOMA && 'bg-white'} rounded-full p-3`}>
           <DiplomaSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.DIPLOMA)} />
         </div>
         <div className={`${curr === SideNavItems.PROJECTS && 'bg-white'} rounded-full p-3`}>
           <ProjectsSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.PROJECTS)} />
-        </div>
-        <div className={`${curr === SideNavItems.CONTACT && 'bg-white'} rounded-full p-3`}>
-          <ContactSVG className="w-10 h-10 cursor-pointer" onClick={()=> setCurr(SideNavItems.CONTACT)}/>
         </div>
     </div>
   );
