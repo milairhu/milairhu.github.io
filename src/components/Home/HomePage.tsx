6de4e9b5e0c1d7c4c96a216755ebe18d2a8c1942
milairhu <hugo.milair@gmail.com>
2024-03-31 10:59:20 +0200

    Width and size of right space

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 const HomePage = () => {
   return (
-    <div>
-        <h1>Home Page</h1>
+    <div className='h-full w-full text-white'>
+      <div className='w-full items-center '>
+        <h1 className=' text-4xl font-extrabold text-left'>Hugo MILAIR</h1>
+      </div>
     </div>
   );
 }
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -16,17 +16,20 @@ const MainPage: React.FC<MainPageProps> = ({tab}) => {
         <div className="flex h-full w-full bg-slate-800 items-center ">
           <div className='h-full ml-8 flex items-center space-x-4'>
             <SideNav currTab={curr} tabSetter={setCurr} />
-            <div className='h-4/6  border-r border-white '/>
+            <div className='h-4/6 border-r border-white'/>
           </div>
-          <div className=" h-full w-full">
-            {
-                curr === TabEnum.HOME ? <HomePage />
-                :
-                curr === TabEnum.DIPLOMA? <ResumePage />
-                :
-                curr === TabEnum.PROJECTS && <HomePage /> 
-            }
+          <div className='flex w-full h-full justify-center items-center'>
+            <div className=" h-5/6 w-11/12">
+              {
+                  curr === TabEnum.HOME ? <HomePage />
+                  :
+                  curr === TabEnum.DIPLOMA? <ResumePage />
+                  :
+                  curr === TabEnum.PROJECTS && <HomePage /> 
+              }
           </div>
+          </div>
+          
         </div>
         
     )
