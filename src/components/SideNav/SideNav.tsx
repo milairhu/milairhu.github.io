8d71e928090f29f1cde9f6e7a0c7e78dbb38586f
milairhu <hugo.milair@gmail.com>
2024-04-11 08:47:50 +0200

    Responsive SideNav for Smartphone

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -15,15 +15,15 @@ const SideNav: React.FC<SideNavProps> = ({ currTab, tabSetter }) => {
 
 
   return (
-      <div className=" bg-slate-300 rounded-full h-1/2 px-2  flex flex-col justify-evenly items-center">
+      <div className=" bg-slate-300 rounded-full h-2/6 md:h-1/2 px-1 md:px-2  flex flex-col justify-evenly items-center">
         <div className={`${currTab === TabEnum.HOME && 'bg-white'} rounded-full w-fit p-2 `}>
-          <Link to ="/"><HomeSVG className="w-10 h-10 cursor-pointer" onClick={()=> tabSetter(TabEnum.HOME)}/></Link>
+          <Link to ="/"><HomeSVG className=" w-6 h-6 md:w-8 md:h-8 cursor-pointer" onClick={()=> tabSetter(TabEnum.HOME)}/></Link>
         </div>
         <div className={`${currTab === TabEnum.DIPLOMA && 'bg-white'} rounded-full  w-fit p-2`}>
-          <Link to ="/resume"><DiplomaSVG className="w-10 h-10 cursor-pointer" onClick={()=> tabSetter(TabEnum.DIPLOMA)} /></Link>
+          <Link to ="/resume"><DiplomaSVG className="w-6 h-6 md:w-8 md:h-8 cursor-pointer" onClick={()=> tabSetter(TabEnum.DIPLOMA)} /></Link>
         </div>
         <div className={`${currTab === TabEnum.PROJECTS && 'bg-white'} rounded-full  w-fit p-2`}>
-          <Link to ="/projects"><ProjectsSVG className="w-10 h-10 cursor-pointer" onClick={()=> tabSetter(TabEnum.PROJECTS)} /></Link>  
+          <Link to ="/projects"><ProjectsSVG className="w-6 h-6 md:w-8 md:h-8 cursor-pointer" onClick={()=> tabSetter(TabEnum.PROJECTS)} /></Link>  
         </div>
     </div>
   );
